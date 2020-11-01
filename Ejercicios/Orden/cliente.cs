b3ffc37ee0207e1c9ad4a72ffe942f01223f866c
carlosrivera1995 <carlosrivera191995@gmail.com>
2020-10-31 18:17:05 -0600

    completo

---
+++ Ejercicios/Orden/cliente.cs
@@ -0,0 +1,10 @@
+﻿public class Cliente: Persona
+{
+    public double Saldo { get; set; }
+    public Cliente(int codigo, string nombre, string telefono)
+    {
+        Codigo = codigo;
+        Nombre = nombre;
+        Telefono = telefono;
+    }
+}
